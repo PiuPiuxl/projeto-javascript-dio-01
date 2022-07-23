0d228a143bd1e85e35741fd2779a61f1ced3b26e
PiuPiuxl <84431863+PiuPiuxl@users.noreply.github.com>
2022-07-23 15:51:32 -0300

    projeto javascript dio

---
+++ js/main.js
@@ -0,0 +1,61 @@
+
+/*
+var d = new Date();
+alert(d.getMonth()+1);
+*/
+
+/*
+var count;
+for(count=0; count <= 5 ;count++){
+    alert(count);
+};
+*/
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+/*
+var idade = prompt ("Qual sua idade");
+if (idade >= 18){
+   alert("Maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+/*
+var frutas = [{nome: "maça" , cor: "vermelha"}, {nome: "uva" , cor: "roxa"}]
+console.log (frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta = {nome: "maça" , cor: "vermelha"}
+console.log (fruta.nome)
+alert(fruta.cor)
+*/
+
+//var lista = ["maça" , "pera" , "laranja"];
+//lista.push("Uva")
+//lista.pop ();
+
+//console.log (lista);
+//console.log (lista.toString());
+//console.log (lista.join("-"));
+
+
+var nome = "Cristopher Leonardo";
+var idade = "17";
+var idade2 = "20";
+var frase = "Malibu é a melhor cidade do fivem";
+alert (nome + " tem " + idade + "anos")
+//alert (idade + idade2);
+console.log (nome);
+console.log (idade + idade2);
+console.log (frase.toLocaleUpperCase());
+alert (frase.replace ("melhor", ("pior")));
